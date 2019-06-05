b34c96f6a72028ceb389ece66b0b7d51d59ccefd
zhangyan <zhangyan@sxygsj.com>
2019-06-05 09:06:12 +0800

    测试

--- src/main/java/com/tut/edu/controller/TestController.java
+++ src/main/java/com/tut/edu/controller/TestController.java
@@ -3,18 +3,52 @@ package com.tut.edu.controller;
 import com.tut.edu.Service.IUserService;
 import com.tut.edu.UserModel.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.util.List;
 
 @RestController
 public class TestController {
     @Autowired
     private IUserService iUserService;
+
     @RequestMapping("/TestCode")
-    public List<UserModel> Test()
-    {
-      return   iUserService.SelectAll();
+    public List<UserModel> Test() {
+        return iUserService.SelectAll();
+    }
+
+    @RequestMapping(value = "/TestRun", method = RequestMethod.POST)
+    public String TestRun(@RequestBody String codeString) {
+
+        File file = new File("d:/test.py");
+        try {
+            System.out.println(file.createNewFile());
+            if (file.exists()) {
+                FileWriter filewriter = new FileWriter("d:/test.py", false);
+                filewriter.write(codeString);
+                filewriter.close();
+                Process proc = Runtime.getRuntime().exec("py D:\\test.py");// 执行py文件
+                proc.waitFor();
+                // 定义Python脚本的返回值
+                String result = null;
+                // 获取CMD的返回流
+                BufferedInputStream in = new BufferedInputStream(proc.getInputStream());
+                // 字符流转换字节流
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String lineStr;
+                while ((lineStr = br.readLine()) != null) {
+                    result = lineStr;
+                }
+                // 关闭输入流
+                br.close();
+                in.close();
+                return result;
+            }
+            return null;
+        } catch (Exception ex) {
+
+        }
+        return null;
     }
 }
